246fd36c1a45cba9e892dd791b67e95f995e80ae
Alok <alok.kumar.iitbhu@gmail.com>
2015-07-16 00:01:06 +0530

    map root url to po

--- savepo/savepo/urls.py
+++ savepo/savepo/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.http  import HttpResponseRedirect
 urlpatterns = [
+    url(r'^$', lambda r: HttpResponseRedirect('po/')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^po/', include('po.urls')), # ADD THIS NEW TUPLE!
+    url(r'^po/', include('po.urls'),
+
+    #url(r'^', 'po.views.index') # ADD THIS NEW TUPLE!
+    )# ADD THIS NEW TUPLE!
 ]
